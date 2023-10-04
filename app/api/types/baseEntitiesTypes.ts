53ad04b0b310ad96637f9f122af138cf282406fa
pzajaczkowski <demoon.pps@gmail.com>
2023-10-04 11:14:32 +0200

    Fix formatting.

--- app/api/types/baseEntitiesTypes.ts
+++ app/api/types/baseEntitiesTypes.ts
@@ -5,84 +5,84 @@
 /**
  * Represents project entity.
  */
-export interface Project{
-    id: string;
-    name: string;
-    description?: string;
-    ownerId: string;
+export interface Project {
+  id: string;
+  name: string;
+  description?: string;
+  ownerId: string;
 }
 
 /**
  * Represents epic entity.
  */
-export interface Epic{
-    id: string;
-    name: string;
-    description: string;
-    statusId: string;
+export interface Epic {
+  id: string;
+  name: string;
+  description: string;
+  statusId: string;
 }
 
 /**
  * Represents story entity.
  */
-export interface Story{
-    id: string;
-    name: string;
-    description: string;
-    epicId?: string;
-    statusId: string;
+export interface Story {
+  id: string;
+  name: string;
+  description: string;
+  epicId?: string;
+  statusId: string;
 }
 
 /**
  * Represents baseProjectTask entity.
  */
-export interface BaseProjectTask{
-    id: string;
-    taskType: string;
-    name: string;
-    priority: string;
-    statusId: string;
-    description?: string;
-    dueDate?: string;
-    estimatedTime?: EstimatedTime;
-    storyPoints?: number;
-    assigneeId?: string;
-    storyId?: string;
+export interface BaseProjectTask {
+  id: string;
+  taskType: string;
+  name: string;
+  priority: string;
+  statusId: string;
+  description?: string;
+  dueDate?: string;
+  estimatedTime?: EstimatedTime;
+  storyPoints?: number;
+  assigneeId?: string;
+  storyId?: string;
 }
 
 /**
  * Represents bugfixProjectTask entity.
  */
-export interface BugfixProjectTask{
-    id: string;
-    taskType: string;
-    name: string;
-    priority: string;
-    statusId: string;
-    description?: string;
-    dueDate?: string;
-    estimatedTime?: EstimatedTime;
-    storyPoints?: number;
-    assigneeId?: string;
-    storyId?: string;
+export interface BugfixProjectTask {
+  id: string;
+  taskType: string;
+  name: string;
+  priority: string;
+  statusId: string;
+  description?: string;
+  dueDate?: string;
+  estimatedTime?: EstimatedTime;
+  storyPoints?: number;
+  assigneeId?: string;
+  storyId?: string;
 }
 
 /**
  * Represents project task status entity.
  */
-export interface ProjectTaskStatus{
-    id: string;
-    name: string;
+export interface ProjectTaskStatus {
+  id: string;
+  name: string;
 }
 
 /**
  * Represents user details.
  */
-export interface UserDetails{
-    firstName: string;
-    lastName: string;
-    email: string;
-    imageUrl: string;
+export interface UserDetails {
+  firstName: string;
+  lastName: string;
+  email: string;
+  imageUrl: string;
 }
 
 //
@@ -92,42 +92,42 @@ export interface UserDetails{
 /**
  * Represents board entity.
  */
-export interface Board{
-    Tasks: BoardProjectTask[];
-    Stories: BoardStory[];
-    Epics: BoardEpic[];
+export interface Board {
+  Tasks: BoardProjectTask[];
+  Stories: BoardStory[];
+  Epics: BoardEpic[];
 }
 
 /**
  * Represents boardEpic entity.
  */
-export interface BoardEpic{
-    id: string;
-    name: string;
-    statusId: string;
+export interface BoardEpic {
+  id: string;
+  name: string;
+  statusId: string;
 }
 
 /**
  * Represents boardStory entity.
  */
-export interface BoardStory{
-    id: string;
-    name: string;
-    epicId?: string;
-    statusId: string;
+export interface BoardStory {
+  id: string;
+  name: string;
+  epicId?: string;
+  statusId: string;
 }
 
 /**
  * Represents boardProjectTask entity.
  */
-export interface BoardProjectTask{
-    id: string;
-    taskType: string;
-    name: string;
-    priority: string;
-    statusId: string;
-    storyId?: string;
-    assigneeId?: string;
+export interface BoardProjectTask {
+  id: string;
+  taskType: string;
+  name: string;
+  priority: string;
+  statusId: string;
+  storyId?: string;
+  assigneeId?: string;
 }
 
 //
@@ -137,23 +137,23 @@ export interface BoardProjectTask{
 /**
  * Represents estimated time object.
  */
-export  interface  EstimatedTime{
-    minutes: number;
-    hours: number;
-    days: number;
+export interface EstimatedTime {
+  minutes: number;
+  hours: number;
+  days: number;
 }
 
 /**
  * Represents api error response object.
  */
-export interface ApiErrorResponse{
-    errorCode: string;
-    message: string;
+export interface ApiErrorResponse {
+  errorCode: string;
+  message: string;
 }
 
 /**
  * Represents api errors response object.
  */
-export interface ApiErrorsResponse{
-    errors: ApiErrorResponse[];
+export interface ApiErrorsResponse {
+  errors: ApiErrorResponse[];
 }
