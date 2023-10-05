58e0c7b72229d434fc19550ffcbadc0f98a42bcb
pzajaczkowski <demoon.pps@gmail.com>
2023-10-05 17:03:35 +0200

    [CUI-16] Add required packages and fix tailwind.config.ts.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,11 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        my_color: "#4dcb7a",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
