c743c5d350c46a685d1a08a0162195c4613d8138
pzajaczkowski <demoon.pps@gmail.com>
2023-10-05 17:04:26 +0200

    [CUI-16] Modify miniTask component to look like figma design.

--- app/components/board/miniTask.tsx
+++ app/components/board/miniTask.tsx
@@ -1,11 +1,16 @@
-import { TaskType } from "~/api/types/baseEntitiesTypes";
-import type { BaseProjectTask } from "~/api/types/baseEntitiesTypes";
+import { Priority, TaskType } from "~/api/types/baseEntitiesTypes";
 import { Card } from "@nextui-org/card";
 import { Avatar, Spacer } from "@nextui-org/react";
+import { MoreHoriz } from "@mui/icons-material";
 
-type MiniTaskProps = Pick<BaseProjectTask, "name" | "priority" | "taskType"> & {
+type MiniTaskProps = {
+  name: string;
+  priority?: Priority;
+  taskType: MiniTaskType;
   avatarUrl: string;
 };
+
+type MiniTaskType = TaskType | "Story" | "Epic";
 
 export function MiniTask({
   name,
@@ -14,24 +19,30 @@ export function MiniTask({
   avatarUrl,
 }: MiniTaskProps) {
   const type = taskType !== "Base" && taskType;
+  const prio = priority !== null && priority;
 
   return (
     <Card>
-      <div className="flex justify-between">
+      <div className="flex justify-between py-3 px-5">
         <div>
           <div className="flex">
-            <TaskPriority priority={priority} />
-            {type && (
+            {type && <MiniTaskType type={taskType} />}
+            {prio && (
               <>
-                <Spacer x={2} />
-                <TaskType type={taskType} />
+                {type && <Spacer x={2} />}
+                <MiniTaskPriority priority={priority} />
               </>
             )}
           </div>
           <div>{name}</div>
         </div>
-        <div>
-          <Avatar src={avatarUrl} />
+        <div className="flex flex-col justify-center items-center gap-1">
+          <div className="flex justify-center ">
+            <Avatar src={avatarUrl} />
+          </div>
+          <div className="flex justify-center">
+            <MoreHoriz></MoreHoriz>
+          </div>
         </div>
       </div>
     </Card>
@@ -40,14 +51,33 @@ export function MiniTask({
 
 export default MiniTask;
 
-function TaskType({ type }: { type: TaskType }) {
+function MiniTaskType({ type }: { type: MiniTaskType }) {
+  const taskTypeToColorClass: Record<MiniTaskType, string> = {
+    Base: "",
+    Bugfix: "text-amber-500",
+    Story: "text-green-500",
+    Epic: "text-purple-500",
+  };
+
+  const colorClass = taskTypeToColorClass[type];
+
   return (
     <>
-      <div>{type}</div>
+      <div className={`${colorClass} font-semibold`}>{type}</div>
     </>
   );
 }
 
-function TaskPriority({ priority }: { priority: string }) {
-  return <div>{priority}</div>;
+function MiniTaskPriority({ priority }: { priority: Priority }) {
+  const priorityToColorClass: Record<Priority, string> = {
+    VeryHigh: "text-red-500",
+    High: "text-orange-500",
+    Medium: "text-yellow-500",
+    Low: "text-emerald-500",
+    VeryLow: "text-teal-500",
+  };
+
+  const colorClass = priorityToColorClass[priority];
+
+  return <div className={`${colorClass} font-semibold`}>{priority}</div>;
 }
