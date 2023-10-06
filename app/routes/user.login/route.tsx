30a03130fec6490616142866bc2cc6fa6dad6ee8
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-06 12:55:52 +0200

    [CUI-11] Login component

--- app/routes/user.login/route.tsx
+++ app/routes/user.login/route.tsx
@@ -1,3 +1,28 @@
+import React from "react";
+import { Button, Checkbox, Input, Link, Spacer } from "@nextui-org/react";
+import { useNavigate } from "@remix-run/react";
+
 export default function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const handleForget = (e: React.MouseEvent) => {
+    e.preventDefault();
+    navigate("/user/forget");
+  };
+
+  return (
+    <form className="max-w-xl grow">
+      <Input size="lg" type="email" label="Email" />
+      <Spacer y={5} />
+      <Input size="lg" type="password" label="Password" />
+      <div className="flex align-middle justify-between my-5">
+        <Checkbox size="lg">Remember me</Checkbox>
+        <Link size="lg" href="/user/forget" onClick={handleForget}>
+          Forgot password
+        </Link>
+      </div>
+      <Button size="lg" color="primary">
+        Login
+      </Button>
+    </form>
+  );
 }
