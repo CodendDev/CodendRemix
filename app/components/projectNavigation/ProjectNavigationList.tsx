b5e9e32337a71e6d450f94d4cf2da0d1d8eedb1a
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-09 19:06:11 +0200

    [CUI-8] Project navigation bar project list

--- app/components/projectNavigation/ProjectNavigationList.tsx
+++ app/components/projectNavigation/ProjectNavigationList.tsx
@@ -1,15 +1,72 @@
 import type { Project } from "~/api/types/baseEntitiesTypes";
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import React, { useState } from "react";
+import { useNavigate } from "@remix-run/react";
 
 type ProjectNavigationListProps = {
   projects: Project[];
   selectedProjectId?: string;
 };
 
-export function ProjectNavigationList({
+export function ProjectNavigationList(props: ProjectNavigationListProps) {
+  return (
+    <Accordion defaultExpandedKeys={["Projects"]} className="text-center">
+      <AccordionItem key="Projects" aria-label="Projects" title="Projects">
+        <ProjectList {...props} />
+      </AccordionItem>
+    </Accordion>
+  );
+}
+
+function ProjectList({
   projects,
   selectedProjectId,
 }: ProjectNavigationListProps) {
-  return <>{JSON.stringify(projects)}</>;
+  const [selected, setSelected] = useState<string | undefined>(
+    selectedProjectId
+  );
+
+  return (
+    <div className="flex flex-col">
+      {projects.map((project, i) => (
+        <ProjectListItem
+          key={i}
+          {...project}
+          selected={project.id == selected}
+          setSelect={() => setSelected(project.id)}
+        />
+      ))}
+    </div>
+  );
+}
+
+function ProjectListItem({
+  id,
+  name,
+  selected,
+  setSelect,
+}: Omit<Project, "ownerId"> & {
+  selected: boolean;
+  setSelect: () => void;
+}) {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => {
+        navigate(`/project/${id}`);
+        setSelect();
+      }}
+      className={`cursor-pointer p-2 text-center hover:bg-gray-100 
+      ${
+        selected
+          ? "border-r-5 border-gray-400 bg-gray-100 hover:bg-gray-200"
+          : "border-r-5"
+      }`}
+    >
+      {name}
+    </div>
+  );
 }
 
 export default ProjectNavigationList;
