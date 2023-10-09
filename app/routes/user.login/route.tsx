60fe19cddb9c96bdfc3467de6b58b9f158090311
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-09 14:27:43 +0200

    [CUI-11] Login error handling

--- app/routes/user.login/route.tsx
+++ app/routes/user.login/route.tsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import { Button, Checkbox, Input, Link, Spacer } from "@nextui-org/react";
-import { useNavigate } from "@remix-run/react";
+import { useActionData, useNavigate } from "@remix-run/react";
 import type { ActionFunctionArgs } from "@remix-run/node";
 import type { handleLoginRequest } from "~/actions/handleLogin";
 import handleLogin from "~/actions/handleLogin";
+import { json } from "@remix-run/node";
+import type { ApiErrorResponse } from "~/api/types/apiErrorsTypes";
+import { ApiErrors } from "~/api/types/apiErrorsTypes";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
+  const errorHandler = (errors: ApiErrorResponse[]) => json({ errors });
+
   const formData = await request.formData();
 
   const data = Object.fromEntries(formData);
   if (data.email === undefined || data.password === undefined) {
-    return false;
+    return errorHandler([]);
   }
 
   const loginRequest: handleLoginRequest = {
     email: data.email.toString(),
     password: data.password.toString(),
     remember: data.remember === "true",
+    errorHandler,
   };
 
   return handleLogin(loginRequest);
@@ -29,12 +35,36 @@ export default function Login() {
     navigate("/user/forget");
   };
 
+  const actionData = useActionData<typeof action>() as unknown as {
+    errors?: ApiErrorResponse[];
+  };
+  const error = actionData?.errors?.hasError(
+    ApiErrors.LoginErrors.InvalidEmailOrPassword
+  );
+
+  console.log(error);
+
   const [checked, setChecked] = useState<boolean>(false);
   return (
     <form className="max-w-lg grow px-5" method="post">
-      <Input size="lg" type="email" label="Email" name="email" />
+      <Input
+        color={error ? "danger" : "default"}
+        size="lg"
+        type="email"
+        label="Email"
+        name="email"
+        isRequired={true}
+      />
       <Spacer y={5} />
-      <Input size="lg" type="password" label="Password" name="password" />
+      <Input
+        color={error ? "danger" : "default"}
+        size="lg"
+        type="password"
+        label="Password"
+        name="password"
+        isRequired={true}
+        errorMessage={error && "Invalid email or password"}
+      />
       <div className="flex align-middle justify-between my-5">
         <Checkbox
           size="lg"
