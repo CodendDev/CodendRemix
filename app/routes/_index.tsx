c4e2adac73f64aa383cf8e48535b4b51f93cba55
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-10-13 13:35:00 +0200

    [CUI-20] Redirect user if not logged in

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useState } from "react";
+import { Link } from "@remix-run/react";
 import { Button } from "@nextui-org/react";
 
 export const meta: MetaFunction = () => {
@@ -10,17 +10,11 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const [dark, setDark] = useState<"dark" | "">("dark");
-
   return (
-    <div className={dark}>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button
-        className="bg-white dark:bg-gray-800"
-        onClick={() => setDark((prev) => (prev === "" ? "dark" : ""))}
-      >
-        button
-      </Button>
+    <div>
+      <Link to="/user">
+        <Button>User account</Button>
+      </Link>
     </div>
   );
 }
