e55feb1468a773923e4aaab60b010684a40c69f0
pzajaczkowski <demoon.pps@gmail.com>
2023-10-30 19:31:37 +0100

    [CUI-24] Modify backlog route to fetch data and use backlog component.

--- app/routes/project.$projectId.backlog/route.tsx
+++ app/routes/project.$projectId.backlog/route.tsx
@@ -1,3 +1,28 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import getToken from "~/actions/getToken";
+import { getBacklog } from "~/api/methods/project";
+import { defer, redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import Backlog from "~/components/backlog/Backlog";
+
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  const token = await getToken(request);
+  if (token === undefined) {
+    redirect("/user/login");
+  }
+  const backlogPromise = getBacklog({
+    projectId: params.projectId!,
+    token: token!,
+  });
+
+  return defer({ backlogPromise });
+};
+
 export default function BacklogPage() {
-  return <>Backlog</>;
+  const loaderData = useLoaderData<typeof loader>();
+  // @ts-ignore
+  const { backlogPromise } = loaderData;
+
+  // @ts-ignore
+  return <Backlog backlogPromise={backlogPromise} />;
 }
