95b2040a8d39ac329a2a7d0d9224880d5613ac2c
pzajaczkowski <demoon.pps@gmail.com>
2023-10-30 19:31:18 +0100

    [CUI-24] Add backlog component

---
+++ app/components/backlog/Backlog.tsx
@@ -0,0 +1,51 @@
+import React, { Suspense, useState } from "react";
+import type { BacklogType } from "~/api/types/baseEntitiesTypes";
+import { Await } from "@remix-run/react";
+import BacklogTask from "~/components/backlog/BacklogTask";
+
+type BacklogProps = {
+  backlogPromise: Promise<BacklogType>;
+};
+
+export function Backlog({ backlogPromise }: BacklogProps) {
+  return (
+    <Suspense fallback={<BacklogLoading />}>
+      <Await resolve={backlogPromise}>
+        {(backlog) => AwaitedBacklog({ backlog })}
+      </Await>
+    </Suspense>
+  );
+}
+
+export default Backlog;
+
+function BacklogLoading() {
+  return <>Loading...</>;
+}
+
+function AwaitedBacklog({ backlog }: { backlog: BacklogType }) {
+  const [selectedBacklogTaskId, setSelectedBacklogTaskId] = useState<
+    string | null
+  >(null);
+
+  return (
+    <div className="px-6 py-1">
+      <div className="px-6 py-2 font-bold text-gray-700">
+        Backlog{" "}
+        <span className="font-normal text-gray-400">
+          ({backlog.tasks.length} tasks)
+        </span>
+      </div>
+      <div className="outl flex flex-col justify-between gap-1 rounded-lg outline-dashed outline-1 outline-offset-4 outline-gray-400">
+        {backlog.tasks.map((task) => (
+          <BacklogTask
+            key={task.id}
+            {...task}
+            selectedBacklogTaskId={selectedBacklogTaskId}
+            setSelectedBacklogTaskId={setSelectedBacklogTaskId}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
