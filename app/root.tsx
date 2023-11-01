2e4ead1fc6e9c08cd4d8f97fe64cfcf9351d4e36
pzajaczkowski <demoon.pps@gmail.com>
2023-11-01 17:41:16 +0100

    [CUI-30] Set custom component as last resort error value.

--- app/root.tsx
+++ app/root.tsx
@@ -6,11 +6,30 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 import styles from "./tailwind.css";
 import { NextUIProvider } from "@nextui-org/react";
+import CustomError from "~/components/errors/CustomError";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return (
+    <html className="m-0 h-full w-full">
+      <head>
+        <title>Something went wrong</title>
+        <Meta />
+        <Links />
+      </head>
+      <body className="m-0 h-full w-full">
+        <CustomError />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
 
 export default function App() {
   return (
