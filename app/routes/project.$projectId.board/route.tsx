680f40d0045a2f293354d4d4af97cca16af47558
pzajaczkowski <demoon.pps@gmail.com>
2023-11-06 13:19:53 +0100

    [CUI-30] Write example inner error boundary

--- app/routes/project.$projectId.board/route.tsx
+++ app/routes/project.$projectId.board/route.tsx
@@ -4,7 +4,10 @@ import getToken from "~/actions/getToken";
 import { getBoard } from "~/api/methods/project";
 import { defer, redirect } from "@remix-run/node";
 import { getProjectTaskStatuses } from "~/api/methods/projectTaskStauses";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useRouteError } from "@remix-run/react";
+import NotFoundError from "~/components/errors/NotFoundError";
+import CustomError from "~/components/errors/CustomError";
+import React from "react";
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   const token = await getToken(request);
@@ -23,6 +26,12 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
 
   return defer({ boardPromise, statusesPromise });
 };
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  // @ts-ignore
+  return <>{error.statusCode === 404 ? <NotFoundError /> : <CustomError />}</>;
+}
 
 export default function BoardPage() {
   const loaderData = useLoaderData<typeof loader>();
