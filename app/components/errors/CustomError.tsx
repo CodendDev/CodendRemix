0436241c5f04b94f7712cca306474a23347f5d7a
pzajaczkowski <demoon.pps@gmail.com>
2023-11-06 13:19:01 +0100

    [CUI-30] Modify CustomError.tsx appearance.

--- app/components/errors/CustomError.tsx
+++ app/components/errors/CustomError.tsx
@@ -2,7 +2,7 @@ import { GiSkullCrack } from "react-icons/gi/index.js";
 
 export default function CustomError() {
   return (
-    <div className="m-auto mt-unit-2xl flex h-1/2 min-h-[250px] w-1/2 min-w-fit flex-col items-center justify-center gap-5 rounded-lg border-3 border-danger-300 bg-danger-50 p-5">
+    <div className="m-auto mt-unit-2xl flex h-1/2 min-h-[270px] w-1/2 min-w-fit flex-col items-center justify-center gap-5 rounded-lg border-3 border-danger-300 bg-danger-50 p-5">
       <div className="text-9xl text-danger-500 shadow-danger">
         <GiSkullCrack />
       </div>
