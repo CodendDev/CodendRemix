4dabd3282637e9bace221429e4bf0f2054dc5a5b
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-07 13:30:46 +0100

    [CUI-35] Create sprint api method

--- app/api/methods/sprint.ts
+++ app/api/methods/sprint.ts
@@ -1,5 +1,9 @@
-import type { SprintsResponse, SprintsRequest } from "~/api/types/sprintTypes";
-import { getAxiosInstance } from "~/api/axios";
+import type {
+  SprintsResponse,
+  SprintsRequest,
+  CreateSprintRequest,
+} from "~/api/types/sprintTypes";
+import { getApiErrorsFromError, getAxiosInstance } from "~/api/axios";
 
 export async function getSprints({
   projectId,
@@ -12,5 +16,22 @@ export async function getSprints({
     return response.data;
   } catch (err) {
     return undefined;
+  }
+}
+
+export async function createSprint(request: CreateSprintRequest) {
+  const { name, startDate, endDate, goal, projectId, token } = request;
+  const axios = getAxiosInstance(token);
+
+  try {
+    const response = await axios.post(`/api/projects/${projectId}/sprints`, {
+      name,
+      startDate: `${startDate}T00:01:00.000Z`,
+      endDate: `${endDate}T00:00:00.000Z`,
+      goal: goal.length === 0 ? null : goal,
+    });
+    return response.data;
+  } catch (err) {
+    return getApiErrorsFromError(err);
   }
 }
--- app/api/types/sprintTypes.ts
+++ app/api/types/sprintTypes.ts
@@ -3,11 +3,19 @@ import type { BoardTask } from "~/api/types/baseEntitiesTypes";
 
 export interface SprintsRequest extends ProjectRequest {}
 
+export interface CreateSprintRequest extends SprintsRequest {
+  name: string;
+  startDate: string;
+  endDate: string;
+  goal: string;
+}
+
 export interface SprintResponse {
   id: string;
   name: string;
   startDate: string;
   endDate: string;
+  goal: string;
   sprintTasks: BoardTask[];
 }
 
