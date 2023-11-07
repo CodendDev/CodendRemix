2f1efceab08d5bf1a8a035770dd8591e4d578531
pzajaczkowski <demoon.pps@gmail.com>
2023-11-07 20:13:49 +0100

    [CUI-15] Add projectTaskStatuses actions.

---
+++ app/api/routes/projectTaskStatus/projectTaskStatusCreateAction.tsx
@@ -0,0 +1,36 @@
+import { ActionFunctionArgs, json } from "@remix-run/node";
+import { ApiErrorResponse } from "~/api/types/apiErrorsTypes";
+import { createProjectTaskStatus } from "~/api/methods/projectTaskStauses";
+import getToken from "~/actions/getToken";
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  const body = await request.formData();
+  const data = Object.fromEntries(body);
+  const errorHandler = (errors: ApiErrorResponse[]) => json({ errors });
+  const token = await getToken(request);
+  const projectId = params.projectId!;
+
+  if (request.method !== "POST") {
+    return;
+  }
+
+  if (data.name === undefined) {
+    return errorHandler([]);
+  }
+
+  const createStatusResponse = await createProjectTaskStatus({
+    projectId: projectId,
+    token: token!,
+    request: {
+      name: data.name.toString().trim(),
+    },
+  });
+
+  if (
+    createStatusResponse === undefined ||
+    createStatusResponse.errors !== undefined
+  ) {
+    return errorHandler(createStatusResponse?.errors ?? []);
+  }
+  return;
+};
