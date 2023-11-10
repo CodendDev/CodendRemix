0d015a37963987e92c15fbc98e1376001b3448ac
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-10 17:42:58 +0100

    [CUI-26] Update sprint route method

--- app/routes/project.$projectId.sprints.$sprintId/route.tsx
+++ app/routes/project.$projectId.sprints.$sprintId/route.tsx
@@ -1,11 +1,13 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { deleteSprint } from "~/api/methods/sprint";
+import { deleteSprint, updateSprint } from "~/api/methods/sprint";
 import getToken from "~/actions/getToken";
 
 export const action = async (args: ActionFunctionArgs) => {
   switch (args.request.method) {
     case "DELETE":
       return actionDelete(args);
+    case "PUT":
+      return actionPut(args);
   }
 };
 
@@ -19,4 +21,26 @@ const actionDelete = async ({ request, params }: ActionFunctionArgs) => {
   }
 
   return await deleteSprint({ token, projectId, sprintId });
+};
+
+const actionPut = async ({ request, params }: ActionFunctionArgs) => {
+  const token = await getToken(request);
+  const projectId = params.projectId;
+  const sprintId = params.sprintId;
+
+  if (!token || !projectId || !sprintId) {
+    return;
+  }
+
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const sprint = {
+    name: data.Name.toString(),
+    startDate: data.StartDate.toString(),
+    endDate: data.EndDate.toString(),
+    goal: data.Goal.toString().trim(),
+    projectId: projectId,
+  };
+
+  return await updateSprint({ token, sprintId, ...sprint });
 };
