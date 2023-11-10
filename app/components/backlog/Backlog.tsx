1cb10c53aa2cc589ff017f1d58f7555a0270a085
pzajaczkowski <demoon.pps@gmail.com>
2023-11-10 22:32:26 +0100

    [CUI-34] Add new route for selected task in backlog

--- app/components/backlog/Backlog.tsx
+++ app/components/backlog/Backlog.tsx
@@ -44,33 +44,25 @@ function AwaitedBacklog({ backlog }: { backlog: BacklogType }) {
   const [selectedBacklogTaskId, setSelectedBacklogTaskId] = useState<
     string | undefined
   >(undefined);
-  const getSelectedTask = () =>
-    backlog.tasks.find((task) => task.id === selectedBacklogTaskId);
 
   return (
-    <>
-      <div className="w-full px-6 py-2">
-        <div className="px-6 py-2 font-bold text-gray-700">
-          Backlog
-          <span className="ml-1 font-normal text-gray-400">
-            ({backlog.tasks.length} tasks)
-          </span>
-        </div>
-        <div className="flex max-h-[calc(100vh-6rem)] min-h-0 min-w-0 flex-shrink-0 flex-col justify-between gap-1 overflow-auto rounded-lg p-1 outline-dashed outline-1 outline-offset-1 outline-gray-400">
-          {backlog.tasks.map((task) => (
-            <BacklogTask
-              key={task.id}
-              {...task}
-              selectedBacklogTaskId={selectedBacklogTaskId}
-              setSelectedBacklogTaskId={setSelectedBacklogTaskId}
-            />
-          ))}
-        </div>
+    <div className="w-full flex-grow">
+      <div className="px-6 pb-2 font-bold text-gray-700">
+        Backlog
+        <span className="ml-1 font-normal text-gray-400">
+          ({backlog.tasks.length} tasks)
+        </span>
       </div>
-      <TaskSidebar
-        taskId={selectedBacklogTaskId}
-        taskType={getSelectedTask()?.taskType}
-      />
-    </>
+      <div className="flex max-h-[calc(100vh-6rem)] min-h-0 min-w-[10rem] flex-shrink-0 flex-col justify-between gap-1 overflow-auto rounded-lg p-1 outline-dashed outline-1 outline-offset-1 outline-gray-400">
+        {backlog.tasks.map((task) => (
+          <BacklogTask
+            key={task.id}
+            {...task}
+            selectedBacklogTaskId={selectedBacklogTaskId}
+            setSelectedBacklogTaskId={setSelectedBacklogTaskId}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
---
+++ app/routes/project.$projectId.backlog.$projectTaskId.$taskType/route.tsx
@@ -0,0 +1,63 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import getToken from "~/actions/getToken";
+import { defer, redirect } from "@remix-run/node";
+import { Outlet, useLoaderData, useRouteError } from "@remix-run/react";
+import NotFoundError from "~/components/errors/NotFoundError";
+import CustomError from "~/components/errors/CustomError";
+import React from "react";
+import { getProjectTask } from "~/api/methods/projectTask";
+import TaskSidebar from "~/components/taskSidebar/TaskSidebar";
+import { ProjectTask } from "~/api/types/baseEntitiesTypes";
+import { getProjectTaskStatuses } from "~/api/methods/projectTaskStauses";
+
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  const token = await getToken(request);
+  if (!token) {
+    redirect("/user/login");
+  }
+  const projectId = params.projectId!;
+  const projectTaskId = params.projectTaskId!;
+  const taskType = params.taskType!;
+
+  const projectTaskStatusesResponse = await getProjectTaskStatuses({
+    projectId: projectId,
+    token: token!,
+  });
+
+  switch (taskType.toLowerCase()) {
+    case "bugfix":
+    case "base": {
+      const projectTaskPromise = getProjectTask({
+        projectId: projectId,
+        token: token!,
+        projectTaskId: projectTaskId,
+      });
+
+      return defer({ projectTaskPromise, projectTaskStatusesResponse });
+    }
+  }
+  return undefined;
+};
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  // @ts-ignore
+  return <>{error.statusCode === 404 ? <NotFoundError /> : <CustomError />}</>;
+}
+
+export default function BoardPage() {
+  const loaderData = useLoaderData<typeof loader>();
+
+  // @ts-ignore
+  const { projectTaskPromise, projectTaskStatusesResponse } = loaderData;
+
+  return (
+    <>
+      <TaskSidebar
+        projectTaskPromise={projectTaskPromise}
+        projectTaskStatusesResponse={projectTaskStatusesResponse}
+      />
+      <Outlet />
+    </>
+  );
+}
