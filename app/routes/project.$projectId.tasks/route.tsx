22038d773ee70a81c04326e22dee450c793a4d61
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-12 12:50:48 +0100

    [CUI-25] My tasks project board

--- app/routes/project.$projectId.tasks/route.tsx
+++ app/routes/project.$projectId.tasks/route.tsx
@@ -1,3 +1,24 @@
+import { Outlet, useLoaderData } from "@remix-run/react";
+import ProjectBoardSprintSelector from "~/components/board/ProjectBoardSprintSelector";
+import React from "react";
+import { loader as BoardLoader } from "~/routes/project.$projectId.board/route";
+
+export const loader = BoardLoader;
+
 export default function TasksPage() {
-  return <>Tasks</>;
+  const loaderData = useLoaderData<typeof loader>();
+
+  // @ts-ignore
+  const { sprintsPromise, projectId } = loaderData;
+
+  return (
+    <div className="flex h-full w-full flex-col">
+      <ProjectBoardSprintSelector
+        sprintsPromise={sprintsPromise}
+        route={`/project/${projectId}/tasks`}
+        noSprintsComponent={<>Select sprint to see tasks assigned to you.</>}
+      />
+      <Outlet />
+    </div>
+  );
 }
