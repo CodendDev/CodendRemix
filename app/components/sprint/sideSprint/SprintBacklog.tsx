0148d0511afa18a6741e99048063114f26a0fc3d
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-13 13:40:59 +0100

    [CUI-39] Side sprint backlog tasks

--- app/components/sprint/sideSprint/SprintBacklog.tsx
+++ app/components/sprint/sideSprint/SprintBacklog.tsx
@@ -2,8 +2,12 @@ import type {
   BacklogType,
   BacklogTaskType,
 } from "~/api/types/baseEntitiesTypes";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Await } from "@remix-run/react";
+import { GrAdd } from "react-icons/gr/index.js";
+import { taskTypeToColorClass } from "~/components/utils/TypeToColor";
+import { AiOutlineCheck, AiOutlineSave } from "react-icons/ai/index.js";
+import { Button } from "@nextui-org/react";
 
 interface SprintBacklogProps {
   backlogPromise: Promise<BacklogType>;
@@ -20,14 +24,77 @@ export default function SprintBacklog(props: SprintBacklogProps) {
 
 const AwaitedBacklog = ({
   backlog,
-}: SprintBacklogProps & { backlog: BacklogType }) => (
-  <div className="px-4">
-    {backlog.tasks.map((task, i) => (
-      <BacklogTask key={i} {...task} />
-    ))}
-  </div>
-);
+}: SprintBacklogProps & { backlog: BacklogType }) => {
+  const [checkedTasks, setCheckedTasks] = useState<string[]>([]);
+  const handleCheck = (id: string, check: boolean) => {
+    const c_checkedTasks = checkedTasks;
+    if (check) {
+      c_checkedTasks.push(id);
+    } else {
+      c_checkedTasks.splice(c_checkedTasks.indexOf(id), 1);
+    }
 
-const BacklogTask = (task: BacklogTaskType) => {
-  return <>{JSON.stringify(task)}</>;
+    setCheckedTasks(c_checkedTasks);
+  };
+
+  const handleSave = () => {
+    console.log(checkedTasks);
+  };
+
+  return (
+    <div className="h-full bg-gray-50">
+      <div className="flex items-center justify-between p-4">
+        <div>Chose tasks that you want to add to the sprint</div>
+        <Button
+          color="primary"
+          size="lg"
+          onClick={handleSave}
+          startContent={<AiOutlineSave />}
+        >
+          Save
+        </Button>
+      </div>
+      <div>
+        {backlog.tasks.map((task, i) => (
+          <BacklogTask key={i} {...task} checkTask={handleCheck} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const BacklogTask = ({
+  id,
+  name,
+  statusName,
+  taskType,
+  checkTask,
+}: BacklogTaskType & {
+  checkTask: (taskId: string, check: boolean) => void;
+}) => {
+  const colorClass = taskTypeToColorClass[taskType];
+  const [selected, setSelected] = useState(false);
+  const handleClick = () => {
+    setSelected((prev) => !prev);
+    checkTask(id, !selected);
+  };
+
+  return (
+    <div
+      className={`flex cursor-pointer justify-between px-4 py-1
+       ${selected ? "bg-gray-200" : "hover:bg-gray-100"}`}
+      onClick={handleClick}
+    >
+      <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center pr-2">
+          {selected ? <AiOutlineCheck /> : <GrAdd />}
+        </div>
+        <div className={`pr-1 ${colorClass}`}>{taskType}</div>
+        <div>{name}</div>
+      </div>
+      <div className="flex">
+        <div>{statusName}</div>
+      </div>
+    </div>
+  );
 };
