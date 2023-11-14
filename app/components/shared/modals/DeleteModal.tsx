19b894c5057cea7094e20430177c49900770597c
pzajaczkowski <demoon.pps@gmail.com>
2023-11-14 18:33:08 +0100

    [CUI-34] Modify delete modal to allow user to choose form type.

--- app/components/shared/modals/DeleteModal.tsx
+++ app/components/shared/modals/DeleteModal.tsx
@@ -7,10 +7,11 @@ import {
   ModalHeader,
 } from "@nextui-org/react";
 import React from "react";
-import { useFetcher } from "@remix-run/react";
+import { Form, useFetcher, useNavigate } from "@remix-run/react";
 
 interface DeleteModalProps {
   actionRoute: string;
+  useFetcherForm: boolean;
   deleteName?: string;
   deleteHeader?: string;
   isOpen: boolean;
@@ -19,6 +20,7 @@ interface DeleteModalProps {
 
 export function DeleteModal({
   actionRoute,
+  useFetcherForm = true,
   deleteName,
   deleteHeader,
   isOpen,
@@ -26,42 +28,54 @@ export function DeleteModal({
 }: DeleteModalProps) {
   const fetcher = useFetcher();
 
+  const modalContent = (
+    <ModalContent>
+      {(onClose) => (
+        <>
+          <ModalHeader className="flex flex-col gap-1">
+            {deleteHeader ?? "Delete confirmation"}
+          </ModalHeader>
+          <ModalBody>
+            <div className="flex h-full w-full flex-row flex-nowrap text-lg">
+              <span className="inline whitespace-nowrap text-center">
+                Are you sure you want to delete
+              </span>
+              <span className="mx-2 inline whitespace-nowrap text-center font-bold text-danger-500">
+                {deleteName ?? ""}
+              </span>
+              ?
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="default" onPress={onClose} size="lg">
+              Cancel
+            </Button>
+            <Button
+              id="form-create-status"
+              color="danger"
+              type="submit"
+              onPress={onClose}
+              size="lg"
+            >
+              Delete
+            </Button>
+          </ModalFooter>
+        </>
+      )}
+    </ModalContent>
+  );
+
   return (
     <Modal isOpen={isOpen} onOpenChange={onOpenChange} className="min-w-fit">
-      <ModalContent>
-        {(onClose) => (
-          <fetcher.Form method="delete" action={actionRoute}>
-            <ModalHeader className="flex flex-col gap-1">
-              {deleteHeader ?? "Delete confirmation"}
-            </ModalHeader>
-            <ModalBody>
-              <div className="flex h-full w-full flex-row flex-nowrap text-lg">
-                <span className="inline whitespace-nowrap text-center">
-                  Are you sure you want to delete
-                </span>
-                <span className="mx-2 inline whitespace-nowrap text-center font-bold text-danger-500">
-                  {deleteName ?? ""}
-                </span>
-                ?
-              </div>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="default" onPress={onClose} size="lg">
-                Cancel
-              </Button>
-              <Button
-                id="form-create-status"
-                color="danger"
-                type="submit"
-                onPress={onClose}
-                size="lg"
-              >
-                Delete
-              </Button>
-            </ModalFooter>
-          </fetcher.Form>
-        )}
-      </ModalContent>
+      {useFetcherForm ? (
+        <fetcher.Form method="delete" action={actionRoute}>
+          {modalContent}
+        </fetcher.Form>
+      ) : (
+        <Form method="delete" action={actionRoute}>
+          {modalContent}
+        </Form>
+      )}
     </Modal>
   );
 }
