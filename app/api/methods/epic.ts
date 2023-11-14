0fc1e3a5d12c16eadca51c157d9ab7b299a7f620
pzajaczkowski <demoon.pps@gmail.com>
2023-11-14 18:33:35 +0100

    [CUI-34] Add epic methods and types.

--- app/api/methods/epic.ts
+++ app/api/methods/epic.ts
@@ -1,19 +1,81 @@
 import { ProjectTask } from "~/api/types/baseEntitiesTypes";
 import { getAxiosInstance } from "~/api/axios";
-import { GetEpicRequest } from "~/api/types/epicTypes";
+import {
+  CreateEpicRequest,
+  GetEpicRequest,
+  UpdateEpicRequest,
+} from "~/api/types/epicTypes";
+import { CreateStoryRequest, UpdateStoryRequest } from "~/api/types/storyTypes";
 
 export async function getEpic({
   projectId,
-  epicId,
+  id,
   token,
 }: GetEpicRequest): Promise<ProjectTask | undefined> {
   const axios = getAxiosInstance(token);
 
   try {
-    const response = await axios.get(
-      `/api/projects/${projectId}/epics/${epicId}`
+    const response = await axios.get(`/api/projects/${projectId}/epics/${id}`);
+    return { ...response.data, taskType: "Epic" };
+  } catch (err) {
+    return undefined;
+  }
+}
+
+export async function deleteEpic({
+  projectId,
+  id,
+  token,
+}: GetEpicRequest): Promise<ProjectTask | undefined> {
+  const axios = getAxiosInstance(token);
+
+  try {
+    const response = await axios.delete(
+      `/api/projects/${projectId}/epics/${id}`
     );
     return { ...response.data, taskType: "Epic" };
+  } catch (err) {
+    return undefined;
+  }
+}
+
+export async function createEpic(request: CreateEpicRequest) {
+  const { token, projectId } = request;
+  const axios = getAxiosInstance(token);
+
+  const apiRequest = {
+    name: request.name,
+    description: request.description,
+    statusId: request.statusId,
+  };
+
+  try {
+    const response = await axios.post(
+      `/api/projects/${projectId}/epics`,
+      apiRequest
+    );
+    return response.status;
+  } catch (err) {
+    return undefined;
+  }
+}
+
+export async function updateEpic(request: UpdateEpicRequest) {
+  const { token, id, projectId } = request;
+  const axios = getAxiosInstance(token);
+
+  const apiRequest = {
+    name: request.name,
+    description: request.description,
+    statusId: request.statusId,
+  };
+
+  try {
+    const response = await axios.put(
+      `/api/projects/${projectId}/epics/${id}`,
+      apiRequest
+    );
+    return response.status;
   } catch (err) {
     return undefined;
   }
--- app/api/types/epicTypes.ts
+++ app/api/types/epicTypes.ts
@@ -1,5 +1,13 @@
 import { ProjectRequest } from "~/api/types/projectTypes";
 
 export interface GetEpicRequest extends ProjectRequest {
-  epicId: string;
+  id: string;
 }
+
+export interface CreateEpicRequest extends ProjectRequest {
+  name: string;
+  description: string;
+  statusId: string;
+}
+
+export interface UpdateEpicRequest extends CreateEpicRequest, GetEpicRequest {}
