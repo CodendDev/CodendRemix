d4334fa0055a46bd3a9c57127740cde1c0a8332c
pzajaczkowski <demoon.pps@gmail.com>
2023-11-20 20:36:10 +0100

    [CUI-43] Add create task button in backlog

--- app/components/backlog/Backlog.tsx
+++ app/components/backlog/Backlog.tsx
@@ -1,9 +1,11 @@
 import React, { Suspense, useState } from "react";
 import type { BacklogTaskType } from "~/api/types/baseEntitiesTypes";
-import { Await } from "@remix-run/react";
+import { Await, useNavigate, useParams } from "@remix-run/react";
 import BacklogTask, {
   BacklogTaskLoading,
 } from "~/components/backlog/BacklogTask";
+import { MdAddCircleOutline } from "react-icons/md/index.js";
+import { Button } from "@nextui-org/react";
 
 type BacklogProps = {
   backlogTasksPromise: Promise<BacklogTaskType[]>;
@@ -43,14 +45,33 @@ function AwaitedBacklog({ backlogTasks }: { backlogTasks: BacklogTaskType[] }) {
   const [selectedBacklogTaskId, setSelectedBacklogTaskId] = useState<
     string | undefined
   >(undefined);
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const handlePress = () => {
+    navigate(`/project/${params.projectId!}/backlog/create`);
+  };
 
   return (
     <div className="w-full overflow-x-auto px-6 py-1">
-      <div className="px-6 py-2 font-bold text-gray-700">
-        Backlog
-        <span className="ml-1 font-normal text-gray-400">
-          ({backlogTasks.length} tasks)
-        </span>
+      <div className="flex w-full flex-row gap-1">
+        <div className="py-2 pl-6 font-bold text-gray-700">
+          Backlog
+          <span className="ml-1 font-normal text-gray-400">
+            ({backlogTasks.length} tasks)
+          </span>
+        </div>
+        <div className="flex items-center">
+          <Button
+            isIconOnly
+            radius="full"
+            size="sm"
+            variant="light"
+            onPress={handlePress}
+          >
+            <MdAddCircleOutline className="text-2xl text-primary-500" />
+          </Button>
+        </div>
       </div>
       <div className="flex max-h-[calc(98vh-6rem)] min-h-0 min-w-[10rem] flex-shrink-0 flex-col justify-between gap-1 overflow-auto rounded-lg p-1 outline-dashed outline-1 outline-offset-1 outline-gray-400">
         {backlogTasks.map((task) => (
