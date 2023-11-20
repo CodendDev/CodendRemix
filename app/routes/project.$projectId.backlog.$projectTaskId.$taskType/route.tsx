7198b55f7d954eb57570e3f55fa415dc4f37a3eb
pzajaczkowski <demoon.pps@gmail.com>
2023-11-20 20:45:56 +0100

    [CUI-43] Fix redirects after task delete.

--- app/routes/project.$projectId.backlog.$projectTaskId.$taskType/route.tsx
+++ app/routes/project.$projectId.backlog.$projectTaskId.$taskType/route.tsx
@@ -1,24 +1,35 @@
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { Outlet, useLoaderData, useParams } from "@remix-run/react";
 import React from "react";
 import TaskSidebar from "~/components/taskSidebar/TaskSidebar";
 import {
   action as ProjectTaskAction,
   loader as ProjectTaskLoader,
 } from "~/routes/api/projectTask/projectTaskGetDeletePutAction";
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
 
-export const action = ProjectTaskAction;
+export const action = async (args: ActionFunctionArgs) => {
+  const response = await ProjectTaskAction(args);
+  if (args.request.method === "DELETE") {
+    return redirect(`/project/${args.params.projectId!}/backlog`);
+  }
+  return response;
+};
 
 export const loader = ProjectTaskLoader;
 
 export default function BacklogTaskSidebar() {
   const loaderData = useLoaderData<typeof loader>();
+  const params = useParams();
 
   // @ts-ignore
   const { projectTaskPromise } = loaderData;
 
   return (
     <>
-      <TaskSidebar projectTaskPromise={projectTaskPromise} />
+      <TaskSidebar
+        projectTaskPromise={projectTaskPromise}
+        actionRouteRoot={`/project/${params.projectId!}/backlog`}
+      />
       <Outlet />
     </>
   );
--- app/routes/project.$projectId.board.$sprintId.$projectTaskId.$taskType/route.tsx
+++ app/routes/project.$projectId.board.$sprintId.$projectTaskId.$taskType/route.tsx
@@ -1,24 +1,37 @@
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { Outlet, useLoaderData, useParams } from "@remix-run/react";
 import {
   action as ProjectTaskAction,
   loader as ProjectTaskLoader,
 } from "~/routes/api/projectTask/projectTaskGetDeletePutAction";
 import TaskSidebar from "~/components/taskSidebar/TaskSidebar";
 import React from "react";
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
 
-export const action = ProjectTaskAction;
+export const action = async (args: ActionFunctionArgs) => {
+  const response = await ProjectTaskAction(args);
+  if (args.request.method === "DELETE") {
+    return redirect(
+      `/project/${args.params.projectId!}/board/${args.params.sprintId!}`
+    );
+  }
+  return response;
+};
 
 export const loader = ProjectTaskLoader;
 
 export default function BoardTaskSidebar() {
   const loaderData = useLoaderData<typeof loader>();
+  const params = useParams();
 
   // @ts-ignore
   const { projectTaskPromise } = loaderData;
 
   return (
     <>
-      <TaskSidebar projectTaskPromise={projectTaskPromise} isFixed={true} />
+      <TaskSidebar
+        projectTaskPromise={projectTaskPromise}
+        actionRouteRoot={`/project/${params.projectId!}/board/${params.sprintId!}`}
+      />
       <Outlet />
     </>
   );
