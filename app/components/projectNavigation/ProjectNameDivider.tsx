ea5c443d321265ad110072ce8df1c431ad57815a
pzajaczkowski <demoon.pps@gmail.com>
2023-11-23 19:07:52 +0100

    [CUI-40] Change divider to match style theme.

--- app/components/projectNavigation/ProjectNameDivider.tsx
+++ app/components/projectNavigation/ProjectNameDivider.tsx
@@ -1,12 +1,14 @@
 import { Divider, Skeleton } from "@nextui-org/react";
 import React from "react";
 
-export function ProjectNameDivider({ name }: { name: string }) {
+export function ProjectNameDivider({ name }: { name: string | undefined }) {
   return (
-    <div className="p-2">
-      <Divider orientation="horizontal" />
-      <div className="w-full text-center">{name}</div>
-      <Divider orientation="horizontal" />
+    <div
+      className={`my-2 w-full border-y-1 border-emerald-700 py-2 text-center text-xl ${
+        name ? "text-primary-900" : "text-gray-500"
+      }`}
+    >
+      {name ?? "Project not selected"}
     </div>
   );
 }
