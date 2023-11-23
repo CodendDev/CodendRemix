e5ef4b0b4baf5a11bd9a78f0ded74ed53e7861b5
pzajaczkowski <demoon.pps@gmail.com>
2023-11-23 22:59:24 +0100

    [CUI-40] Add user element on sidebar and account page.

--- app/components/projectNavigation/ProjectNavigationBar.tsx
+++ app/components/projectNavigation/ProjectNavigationBar.tsx
@@ -5,13 +5,13 @@ import ProjectNavigationList, {
 import ProjectNavigationActionsList, {
   LoadingProjectNavigationActionsList,
 } from "~/components/projectNavigation/ProjectNavigationActionsList";
-import { Await, useParams } from "@remix-run/react";
+import { Await, useNavigate, useParams } from "@remix-run/react";
 import React, { Suspense, useState } from "react";
 import ProjectNameDivider, {
   LoadingProjectNameDivider,
 } from "~/components/projectNavigation/ProjectNameDivider";
 import { LuLogOut } from "react-icons/lu/index.js";
-import { Button } from "@nextui-org/react";
+import { Button, User } from "@nextui-org/react";
 
 type ProjectNavigationBarProps = {
   projectsPromise: Promise<PagedResponse<Project>>;
@@ -74,6 +74,7 @@ function AwaitedProjectNavigationBar({
   projectId: string | undefined;
   projects: Project[];
 }) {
+  const navigate = useNavigate();
   const project = projects.find((p) => p.id === projectId);
   const [name, setName] = useState<string | undefined>(project?.name);
 
@@ -86,7 +87,17 @@ function AwaitedProjectNavigationBar({
         />
         <ProjectNameDivider name={name} />
         <ProjectNavigationActionsList projectId={projectId} />{" "}
-        <div className="mb-2 mt-auto flex flex-row px-3">
+        <div className="mb-2 mt-auto flex flex-col gap-2 px-3">
+          <User
+            name="Your account"
+            description="user@email.tododawdwadwadawawda"
+            avatarProps={{ src: "/avatars/1.png" }}
+            className="cursor-pointer py-1 hover:bg-gray-300"
+            classNames={{ description: "overflow-hidden text-ellipsis w-36" }}
+            onClick={() => {
+              navigate("/project/account");
+            }}
+          />
           <Button
             variant="ghost"
             color="primary"
