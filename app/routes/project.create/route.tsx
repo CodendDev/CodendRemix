583235757cdebbd31e8f71843126a85ce102dadf
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-28 13:48:05 +0100

    [CUI-47] Create project route

--- app/routes/project.create/route.tsx
+++ app/routes/project.create/route.tsx
@@ -1,3 +1,33 @@
+import ProjectEditor from "~/components/project/ProjectEditor";
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import { createProject } from "~/api/methods/project";
+import getToken from "~/actions/getToken";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const token = await getToken(request);
+  if (!token) {
+    return undefined;
+  }
+
+  const data = Object.fromEntries(await request.formData());
+  const id = await createProject({
+    token,
+    name: data.name.toString(),
+    description: data.description.toString(),
+  });
+
+  if (id) {
+    return redirect(`/project/${id}`);
+  }
+
+  return undefined;
+};
+
 export default function CreateProjectPage() {
-  return <>Create</>;
+  return (
+    <div>
+      <div>Create project</div>
+      <ProjectEditor />
+    </div>
+  );
 }
