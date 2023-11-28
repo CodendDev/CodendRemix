88745e8be8344cb1a7169a19d6ffb16d914f9fb9
pzajaczkowski <demoon.pps@gmail.com>
2023-11-28 14:35:12 +0100

    [CUI-51] Create route for account details

---
+++ app/routes/user.account/route.tsx
@@ -0,0 +1,10 @@
+import ProjectPage, {
+  action as BarAction,
+  loader as BarLoader,
+} from "~/routes/project/route";
+
+export const action = BarAction;
+
+export const loader = BarLoader;
+
+export default ProjectPage;
