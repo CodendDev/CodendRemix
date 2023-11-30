a6eafdcebb2fd8122c2f2e86a3a82001913bc1cd
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-11-30 16:22:10 +0100

    [CUI-27] Project members route

---
+++ app/components/project/members/ProjectMembers.tsx
@@ -0,0 +1,24 @@
+import { UserDetails } from "~/api/types/baseEntitiesTypes";
+import { Card } from "@nextui-org/card";
+import ProjectMembersList from "~/components/project/members/ProjectMembersList";
+
+interface ProjectMembersProps {
+  members: UserDetails[];
+  ownerId: string;
+}
+
+export function ProjectMembers(props: ProjectMembersProps) {
+  const membersCount =
+    props.members.length === 1 ? "1 member" : `${props.members.length} members`;
+
+  return (
+    <>
+      <div className="w-full p-5 text-3xl text-emerald-800">
+        Project <span className="text-lg">({membersCount})</span>
+      </div>
+      <Card className="w-3/4 min-w-fit">
+        <ProjectMembersList {...props} />
+      </Card>
+    </>
+  );
+}
