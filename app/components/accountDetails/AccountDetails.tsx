2f2879cee2ad2c441d4e757328e66b704473c47e
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-12-02 21:29:34 +0100

    [CUI-54] AccountDetails editor using remix fetcher

--- app/components/accountDetails/AccountDetails.tsx
+++ app/components/accountDetails/AccountDetails.tsx
@@ -1,44 +1,53 @@
-import React, { useContext } from "react";
-import { Button, Link, Spacer } from "@nextui-org/react";
-import { Form, useNavigate } from "@remix-run/react";
-import { UserDetailsContext } from "~/routes/project/route";
+import React from "react";
+import { Button, Spacer } from "@nextui-org/react";
+import { Link, useFetcher } from "@remix-run/react";
 import { AvatarCard } from "~/components/accountDetails/cards/AvatarCard";
 import { DetailsCard } from "~/components/accountDetails/cards/DetailsCard";
+import { UserDetails } from "~/api/types/baseEntitiesTypes";
 
-export function AccountDetails({ editable = false }: { editable?: boolean }) {
-  const { userDetails } = useContext(UserDetailsContext);
-  const navigate = useNavigate();
+interface AccountDetailsProps {
+  userDetails: UserDetails;
+  editable?: boolean;
+}
+export function AccountDetails({
+  editable = false,
+  userDetails,
+}: AccountDetailsProps) {
+  const fetcher = useFetcher();
 
   return (
-    <Form action={"/user/account/edit"} method="PUT">
+    <fetcher.Form action="/user/account/edit" method="PUT">
       <div className="flex w-[20em] min-w-[20em] flex-col justify-center py-4 md:w-[30em]">
         <div className="p-2 text-3xl text-emerald-800">Account details</div>
         <AvatarCard imageUrl={userDetails.imageUrl} editable={editable} />
         <Spacer y={4} />
         <DetailsCard user={userDetails} editable={editable} />
         {editable && (
-          <div className="flex justify-end gap-2 pt-4">
+          <div className="flex justify-between pt-4">
             <Button
               as={Link}
               color="danger"
               variant="solid"
               size="lg"
               className="font-bold"
-              onPress={() => navigate("/user/account/details")}
+              to="/user/account/details"
+              isDisabled={fetcher.state !== "idle"}
             >
-              Cancel
+              Go back
             </Button>
             <Button
               type="submit"
               color="primary"
               size="lg"
               className="font-bold hover:bg-emerald-600"
+              isLoading={fetcher.state !== "idle"}
+              spinnerPlacement="end"
             >
               Apply
             </Button>
           </div>
         )}
       </div>
-    </Form>
+    </fetcher.Form>
   );
 }
